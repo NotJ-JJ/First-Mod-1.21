57c980e825c3d18a8ebe16dc0c49ae675acb6c9d
Simon Wilder <z.notjj@gmail.com>
2025-06-11 21:22:04 +1000

    Blehhhx2

--- src/main/java/net/femboy/tutorialmod/item/ModItems.java
+++ src/main/java/net/femboy/tutorialmod/item/ModItems.java
@@ -1,13 +1,16 @@
 package net.femboy.tutorialmod.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.femboy.tutorialmod.TutorialMod;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    public static final Item FEMBOYITE = registerItem("femboyite",new Item(new Item.Settings()));
+    public static final Item FEMINITE = registerItem("feminite",new Item(new Item.Settings()));
+    public static final Item TESTOSORITE = registerItem("testosorite",new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID,name),item);
@@ -16,6 +19,9 @@ public class ModItems {
     public static void registerModItems() {
         TutorialMod.LOGGER.info("Registering Mod Items for"+TutorialMod.MOD_ID);
 
-
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(FEMINITE);
+            fabricItemGroupEntries.add(TESTOSORITE);
+        });
     }
 }
