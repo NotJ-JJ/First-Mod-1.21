3c67014fae974a8c502dba3c55c205b4db11aff9
Simon Wilder <z.notjj@gmail.com>
2025-06-12 18:45:49 +1000

    Blehhhx2

--- src/main/java/net/femboy/tutorialmod/TutorialMod.java
+++ src/main/java/net/femboy/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.femboy.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.femboy.tutorialmod.block.ModBlocks;
 import net.femboy.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,5 +14,6 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
